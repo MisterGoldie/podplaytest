ff414c78050564f3c109c4af6ae3e079aae43ab3
MisterGoldie <118089324+MisterGoldie@users.noreply.github.com>
2024-10-06 10:30:30 -0500

    fdbrerwebv

--- api/index.tsx
+++ api/index.tsx
@@ -651,7 +651,7 @@ app.frame('/share', async (c) => {
         <h1 style={{ fontSize: '60px', marginBottom: '20px' }}>Thanks for Playing!</h1>
         <p style={{ fontSize: '44px', marginBottom: '20px' }}>Your Record: {userRecord.wins}W - {userRecord.losses}L - {userRecord.ties}T</p>
         <p style={{ fontSize: '40px', marginBottom: '20px' }}>POD Score: {podScore}</p>
-        <p style={{ fontSize: '36px', marginBottom: '20px' }}>Total Games Played: {totalGamesPlayed}</p>
+        <p style={{ fontSize: '36px', marginBottom: '20px' }}>Total Games Played Including Ties: {totalGamesPlayed}</p>
         <p style={{ fontSize: '32px', marginBottom: '20px' }}>Frame by @goldie & @themrsazon</p>
       </div>
     ),
