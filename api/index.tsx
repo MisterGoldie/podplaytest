879f63dc592e7898d1a5527ed28575aee257c25b
MisterGoldie <118089324+MisterGoldie@users.noreply.github.com>
2024-10-06 12:33:00 -0500

    fsvewwavs

--- api/index.tsx
+++ api/index.tsx
@@ -830,7 +830,7 @@ app.frame('/share', async (c) => {
         <p style={{ fontSize: '40px', marginBottom: '20px' }}>POD Score: {podScore}</p>
         <p style={{ fontSize: '36px', marginBottom: '20px' }}>Total Games Played: {totalGamesPlayed}</p>
         <p style={{ fontSize: '32px', marginBottom: '20px' }}>
-          {ownsThepodToken ? `You hold (${thepodTokenBalance.toFixed(2)} Fan tokens)!` : '👀 Get your /Thepod Fan Token!'}
+          {ownsThepodToken ? `You hold ${thepodTokenBalance.toFixed(2)} /Thepod Fan tokens!` : '👀 Get your /Thepod Fan Token!'}
         </p>
         <p style={{ fontSize: '28px', marginBottom: '20px' }}>Frame by @goldie & @themrsazon</p>
       </div>
